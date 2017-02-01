df395258f44b1c37d81cf13b06c67b5e63166042
Kasper Kristensen <kaskr@dtu.dk>
2017-02-01 16:55:35 +0100

    re-structure

--- readHAC/R/readHAC.R
+++ readHAC/R/readHAC.R
@@ -1,3 +1,25 @@
+##' Read acoustic HAC raw data
+##'
+##' The HAC data format is a binary format containing so-called \code{tuples}.
+##' A tuple can hold various sorts of information depending on the tuple type.
+##' For instance tuples exist to specify positions, echosounder information and
+##' acoustic signal data etc.
+##' This R package can read, write and manipulate the HAC data format.
+##' 
+##' \tabular{ll}{ Package: \tab HAC\cr Type: \tab Package\cr Version: \tab
+##' 1.0\cr Date: \tab 2013-01-01\cr License: \tab GPL\cr
+##' LazyLoad: \tab yes\cr } 
+##' 
+##' @name readHAC-package
+##' @aliases readHAC-package
+##' @docType package
+##' @author Kasper Kristensen
+##' 
+##' Maintainer: <kaskr@@imm.dtu.dk>
+##' @references \url{http://biblio.uqar.ca/archives/30005500.pdf}
+##' @example demo/intro.R
+NULL
+
 ## Get all needed HAC definition data:
 ## * tableList:          List of definition tables.
 ## * tuple2text:         Lookup code -> description text
